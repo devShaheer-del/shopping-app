f94f52a74ca751399bc0bdb6a6dc5a9da137f150
Muhmmad shaheer Shah <shaheeraptech1@gmail.com>
2025-02-24 09:05:57 -0800

    finals

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -3,21 +3,20 @@ import { useNavigate } from 'react-router-dom';
 
 function Protected({ Component }) {
     const navigate = useNavigate();
-    const [isAuth, setIsAuth] = useState(false);
+    const [isAuth, setIsAuth] = useState(null); // Initialize with null
 
     useEffect(() => {
-        if (typeof window !== "undefined") {
-            const user = localStorage.getItem("UserName");
-            const token = localStorage.getItem("UserToken");
+        const user = localStorage.getItem("UserName");
+        const token = localStorage.getItem("UserToken");
 
-            if (!user || !token) {
-                navigate('/Login');
-            } else {
-                setIsAuth(true);
-            }
+        if (!user || !token) {
+            navigate('/Login');
+        } else {
+            setIsAuth(true);
         }
     }, [navigate]);
 
+    if (isAuth === null) return null; // Wait for auth check
     return isAuth ? <Component /> : null;
 }
 
