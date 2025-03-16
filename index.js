d4410c96a6581d74cc9ae01e3b847e369fbf7cc7
Muhmmad shaheer Shah <shaheeraptech1@gmail.com>
2025-03-16 04:47:36 -0700

    test agin

--- index.js
+++ index.js
@@ -23,11 +23,6 @@ const productRouter = require('./routes/AddProducts');
 const AdminRouter = require('./routes/AdminRoute');
 const CategoryRouter = require('./routes/categoryRouter');
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "Sever Successfully Deployedd"
-    })
-})
 
 app.use('/contact', contactRouter);
 app.use('/user', userRouter);
